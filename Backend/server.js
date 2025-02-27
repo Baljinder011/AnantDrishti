9e127c2cf04b9d18d9ef0fe3c603470154ede2f4
baljinder <baljinder48623@gmail.com>
2025-02-27 19:02:39 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -81,9 +81,10 @@ app.post("/create-payment-link", async (req, res) => {
             link_auto_reminders: true,
             link_expiry_time: getExpiryTime(),
             link_meta: {
-              return_url: `http://localhost:${PORT}/redirect.html?link_id=${link_id}`
+              // return_url: `https://localhost:${PORT}/redirect.html?link_id=${link_id}`
+              return_url: `          https://indraq.tech/redirect.html?link_id=${link_id}`
+
           }
-          
         };
         
         const response = await axios.post(apiUrl, payload, {
