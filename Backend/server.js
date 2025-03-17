271ad8849d9ac2d570be114721856abc1b25a187
Baljinder011 <singhjasminder743@gmail.com>
2025-03-17 17:59:57 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -36,10 +36,7 @@ app.use(cors({
 }));
 // :white_tick: Manually Set Headers for Debugging
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
+ 
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
